4204210568ab248b036b6abe94e7f2a6b9bfa1ab
lucaskrespi <lucas.krespi@gmail.com>
2021-09-14 18:25:12 -0400

    start button added (debug massage only)

---
+++ Assets/Scripts/StartButtonBehavior.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartButtonBehavior : MonoBehaviour
+{
+    public void OnPressStartButton()
+    {
+        Debug.Log("Start Button Pressed!!");
+    }
+}
