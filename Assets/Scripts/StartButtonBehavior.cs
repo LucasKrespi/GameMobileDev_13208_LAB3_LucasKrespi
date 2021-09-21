bac446cdf831cbd08122db373d6a1bffcfd2b8b5
lucaskrespi <lucas.krespi@gmail.com>
2021-09-21 16:45:43 -0400

    menu and end scene added / buttons added to move between scenes

--- Assets/Scripts/StartButtonBehavior.cs
+++ Assets/Scripts/StartButtonBehavior.cs
@@ -1,11 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class StartButtonBehavior : MonoBehaviour
 {
     public void OnPressStartButton()
     {
-        Debug.Log("Start Button Pressed!!");
+        switch (gameObject.name)
+        {
+            case "Start":
+                SceneManager.LoadScene("main");
+                break;
+            case "Back":
+                SceneManager.LoadScene("menu");
+                break;
+            case "End":
+                SceneManager.LoadScene("end");
+                break;
+            case "Return":
+                SceneManager.LoadScene("menu");
+                break;
+
+        }
+        
     }
 }
